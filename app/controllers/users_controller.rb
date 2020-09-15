d6144fb3878027dbc3eab121b7dcb771ff83885f
Jed Stanton <jedstanton@hotmail.com>
2020-09-15 18:04:56 +0100

    sign up form created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(params.require(:user).permit(:email,
+    :password))
+
+    session[:user_id] = @user.id
+
+    redirect_to '/welcome'
   end
 end
